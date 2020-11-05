4a6cc79ccc63e96ecc962d1e13e11324aea46b64
tarun390 <73883651+tarun390@users.noreply.github.com>
2020-11-05 18:52:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -74,7 +74,7 @@ function draw() {
   if(gameState===PLAY){
     score = score + Math.round(getFrameRate()/60);
   
-    if(keyDown("space") && trex.y>=169) {
+    if(keyDown("space"||"UP_ARROW") && trex.y>=169) {
       trex.velocityY = -10;
     }
 
